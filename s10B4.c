881b8fdb3fc29746c307496a851d151abedabbd8
Lê Nhật Linh <lenhatlinh19042006@gmail.com>
2024-12-04 13:09:53 +0700

    Add files via upload

--- s10B4.c
+++ s10B4.c
@@ -2,16 +2,14 @@
 
 int main(){
 	int arr[8] = {1,6,2,7,9,4,11,5};
-	
 	printf("Mang ban dau: ");
 	for ( int i =0; i< 8; i++){
 		printf("\t%d", arr[i]);
 	}
 
-	//selection sort
 	for ( int i = 0; i< 8; i++){
 		int minIndex = i;
-		for ( int j = i+1; j< 8; j++){
+		for ( int j = i+1; j < 8; j++){
 			if ( arr[j] < arr[minIndex]){
 				minIndex = j;
 			}
